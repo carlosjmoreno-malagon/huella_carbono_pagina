ca1eed4aa3b61b8a01f8122069b08de6cad05f4e
carlosjmoreno-malagon <nacido2004@gmail.com>
2024-10-01 10:37:06 -0500

    Solucion de problemas

--- crud/models.py
+++ crud/models.py
@@ -4,5 +4,4 @@ class project(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
     technology = models.CharField(max_length=200)
-    created_at = models.DateField(auto_now_add=True)
-    
+    created_at = models.DateTimeField(auto_now_add=True)
--- huella_carbon/settings.py
+++ huella_carbon/settings.py
@@ -132,7 +132,7 @@ if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-    
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
