a8ac83c535d495b3cd33a3615930e2d8f3a557a8
carlosjmoreno-malagon <nacido2004@gmail.com>
2024-10-03 09:02:29 -0500

    arreglos

--- crud/urls.py
+++ crud/urls.py
@@ -1,6 +1,6 @@
 from rest_framework import routers
 from .api import projectViewSet
 router = routers.DefaultRouter()
-router.register('api/crud', projectViewSet, 'cruds')
+router.register('api/crud', projectViewSet, 'crud')
 
 urlpatterns = router.urls
